a9654f522102d5df05ad0e231ed54408cf8e9218
Axelbresa <bresanovichaxel43@gmail.com>
2023-07-05 14:47:32 -0300

    se agrego los middlewares

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 // Imports
+const express = require("express");
 const cors = require('cors');
-const express = require('express');
-
 const path = require('path');
+// const helmet = require('helmet')
+const morgan = require ('morgan')
+require("dotenv").config();
+require("ejs");
 
 const app = express();
 
 // Middlewares
 // TODO: Implementar middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,4 +26,4 @@ app.use('/api', require('./routes/reserva.routes'));
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(3000, () => console.log('Server on port xxxx'));
